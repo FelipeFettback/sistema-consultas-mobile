c353357a4d5b73547a5004a2d9a815b6ab3bf627
Felipe Wapf Fettback <wapffelipe@gmail.com>
2026-04-13 10:17:01 -0300

    Adição do componente ConsultaCard

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as ConsultaCard } from "./consultaCard"
+
+ /*
+ * export { default as Loading } from "./Loading";
+ * export { default as EmptyState } from "./EmptyState";
+ * export { default as Header } from "./Header";
+ */
